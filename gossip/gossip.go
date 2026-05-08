b05abb3e010569080c0bd268e772445862cef8fe
leoantony72 <leoantony102@gmail.com>
2026-05-09 00:04:52 +0530

    migrated to protobuf for gossip protocol

--- gossip/gossip.go
+++ gossip/gossip.go
@@ -3,14 +3,21 @@ package gossip
 import (
 	"sync"
 	"time"
+	"iris/serializer/pb"
 )
 
 type NodeHealth int
+type MessageType int
 
 const (
 	ALIVE NodeHealth = iota
 	SUSPECT
 	DEAD
+)
+
+const (
+	INTER_GOSSIP MessageType = iota
+	INTRA_GOSSIP
 )
 
 type NodeState struct {
@@ -20,6 +27,11 @@ type NodeState struct {
 	LastSeen       time.Time
 	SuspicionCount int
 	Version        uint64
+}
+
+type GossipMessage struct {
+	MessageType MessageType
+	
 }
 
 /*
@@ -48,18 +60,18 @@ type Gossip struct {
 		used to update the node states in the gossip protocol, when a nodes is marked as DEAD, the nodeID will
 		sent to this channel to update the GossipTable. The master node will be responsible for senting the dead nodeID to the busport of the current node.
 	*/
-	DeadEvents <- chan string
+	DeadEvents <-chan string
 
 	/*
 		used to update the node states in the gossipTable, when a new node joins the cluster, the nodeID will be sent to this channel. The master node will be responsible for senting the new nodeID to the busport of the current node.
 	*/
-	JoinEvents <- chan NodeState
+	JoinEvents <-chan NodeState
 
 	/*
 		used to forward the incoming gossips(both inter and intra) received through the busport to the gossip protocol, the gossips will be processed and update the gossipTable accordingly.
 	*/
-	IntraGossips <- chan string
-	InterGossips <- chan string
+	IntraGossips <-chan string
+	InterGossips <-chan string
 
 	onDead func(nodeID string, evidenceCount int)
 }
@@ -76,6 +88,7 @@ func NewGossip(view ClusterView, onDead func(nodeID string, evidenceCount int)) 
 		IntraGossips: make(chan string, 10),
 		InterGossips: make(chan string, 10),
 	}
+
 	gossip.seedFromView()
 	return gossip
 }
@@ -102,14 +115,14 @@ func (g *Gossip) seedFromView() {
 }
 
 /*
-	These messges are received by the BUSPORT, sent by the MasterNode to all the nodes in the cluster, when a node is marked as DEAD, and this function will update the gossip table with the new state of the nodeID and also incremnt the version of the nodeID in the gossip table.
+These messges are received by the BUSPORT, sent by the MasterNode to all the nodes in the cluster, when a node is marked as DEAD, and this function will update the gossip table with the new state of the nodeID and also incremnt the version of the nodeID in the gossip table.
 */
-func (g *Gossip) MonitorChannel(){
-	for{
-		select{
-		case nodeID:= <- g.DeadEvents:
+func (g *Gossip) MonitorChannel() {
+	for {
+		select {
+		case nodeID := <-g.DeadEvents:
 			g.mu.Lock()
-			if NodeState, exists := g.table[nodeID];exists{
+			if NodeState, exists := g.table[nodeID]; exists {
 				NodeState.Health = DEAD
 				NodeState.Version += 1
 			}
@@ -123,22 +136,20 @@ func (g *Gossip) MonitorChannel(){
 	}
 }
 
-
 // @ Todo: eth markaruth !!!!!!!!!!!!
 // what happens when a nodeExit message send by the master fails to reach the node ? and in the gossip protocol we receive a message saying there is a newNode in the cluster?
 // HOW Will the node resolve this issue, will it req a snapshot from the master node, allengil accept the data from the gossip protocol and update the goosip table with it.
 // Also if the master sends the nodeExit, and the node pass it to the gossip protocol, it deletes node entry with NodeID, but if the node receives a gossip (before the sending node has the updated cluster state) in which the same nodeID is marked as alive, then the node will update the gossip table with the new state of the nodeID as alive and then the node that is dead will still be alive in the system as a zombies. myre .  ahh no we could use versions for this. fk
 
-
-/* 
-//send only 
+/*
+//send only
 func(send chan<- string){
-	send <- "hi" 
+	send <- "hi"
 }
 
 //receive only
 func(t <- chan string){
 	msg := <-t
-} 
+}
 
 */
--- gossip/inter.go
+++ gossip/inter.go
@@ -1,6 +1,16 @@
-package gossip 
+package gossip
 
-/*Gossips the state of the node to randomly selected nodes in the group same as the current node.
+import (
+	"math/rand"
+)
 
-*/
+/*Gossips the state of the node to randomly selected nodes in the group same as the current node.*/
+func (g *Gossip) InterGossip() {
+	members := g.view.GetGroupMembers(g.Group)
 
+	randomNum := rand.Uint32() * uint32(len(members))
+
+	selectedNode := members[randomNum]
+
+	
+}
